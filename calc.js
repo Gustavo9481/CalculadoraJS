8af33d32c6f354739801412f88363d5910042a35
Gustavo Colmenares Zarache <colmenares9481@gmail.com>
2022-06-26 22:32:17 -0500

    Carga inicial

---
+++ calc.js
@@ -0,0 +1,49 @@
+// Basic Calculator - no User Interface
+
+// Functions for operations
+const sum = (num1,num2) => {
+  return parseInt(num1) + parseInt(num2);
+}
+
+const subtract = (num1,num2) => {
+  return parseInt(num1) - parseInt(num2);
+}
+
+const multiply = (num1,num2) => {
+  return parseInt(num1) * parseInt(num2);
+}
+
+const divide = (num1,num2) => {
+  return parseInt(num1) / parseInt(num2);
+}
+
+
+// run app *****************************************************************
+let num1 = prompt("enter first value");
+let num2 = prompt("enter second value");
+let select = prompt(`enter operation, select an number of option =>
+sum:[1]  subtract:[2]  multiply:[3]  divide:[4]`);
+var result = 0;
+
+// Conditions for selected operation ***************************************
+if (select === "1") {
+  let result = sum(num1,num2);
+  document.write(`<h1>The result is <b style="color:red">${result}</b></h1>`);
+}
+else if (select === "2") {
+  let result = subtract(num1,num2);
+  document.write(`<h1>The result is <b style="color:red">${result}</b></h1>`);
+}
+else if (select === "3") {
+  let result = multiply(num1,num2);
+  document.write(`<h1>The result is <b style="color:red">${result}</b></h1>`);
+}
+else if (select === "4") {
+  let result = divide(num1,num2);
+  document.write(`<h1>The result is <b style="color:red">${result}</b></h1>`);
+}
+else {
+  alert("The selection not exist, try again");
+}
+
+
